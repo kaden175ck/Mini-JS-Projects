7587b857f53fcc4e26d70417df6a5f58c59fa24d
kaden175ck <haoyang.shi@ucalgary.ca>
2024-03-26 17:51:29 -0600

    click and char jumps

--- dinosaur game/script.js
+++ dinosaur game/script.js
@@ -1,3 +1,11 @@
 
 
+// create 2 var to access char and block
+var character = document.getElementById("character");
+var block = document.getElementById("block");
+
+function jump(){
+    character.classList.add("animate");
+    
+}
 
