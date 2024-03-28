f1f5e8282fa72d0f18a5ca48577dc151216c7ce8
kaden175ck <haoyang.shi@ucalgary.ca>
2024-03-28 02:04:58 -0600

    add game logic

--- dinosaur game/script.js
+++ dinosaur game/script.js
@@ -5,7 +5,30 @@ var character = document.getElementById("character");
 var block = document.getElementById("block");
 
 function jump(){
-    character.classList.add("animate");
+    if (character.classList != "animate"){
+        character.classList.add("animate");
+    }
     
+    setTimeout(function(){
+        character.classList.remove("animate");
+    },500)
+    // after 500ms when animation done remove the animation class
+
 }
 
+var checkDead = setInterval(function(){
+    var characterTop = parseInt(window.getComputedStyle(character).getPropertyValue("top"));
+    // you need to parse to int, because it will give you px unit
+    // return the top position of the char
+    var blockLeft = parseInt(window.getComputedStyle(block).getPropertyValue("left"));
+
+    if(blockLeft<20 && blockLeft>0 && characterTop>130){
+        block.style.animation = "none";
+        block.style.display = "none";
+        // so that game stop page must be refreshed
+        alert("You lose!");
+    }
+
+
+}, 10);
+// run the check function every 10ms to see if its dead
