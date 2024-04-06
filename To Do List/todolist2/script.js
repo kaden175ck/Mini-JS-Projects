07a55aca018f31e2e3cdf8c418952776f3de1d4e
kaden175ck <haoyang.shi@ucalgary.ca>
2024-04-05 18:01:35 -0600

    basic JS done, need more functions

--- To Do List/todolist2/script.js
+++ To Do List/todolist2/script.js
@@ -13,5 +13,38 @@ function addTodo (event) {
     event.preventDefault();
 
     // create todo div mentioned in HTML
-    const
+    const todoDiv = document.createElement('div');
+    todoDiv.classList.add('todo');
+
+    // create li mentioned in HTML
+    const newTodo = document.createElement('li');
+    newTodo.innerText = 'hey';
+    // newTodo.innerText = todoInput.value;
+    newTodo.classList.add('todo-item');
+    todoDiv.appendChild(newTodo);
+
+
+    // check mark button
+    const completedButton = document.createElement('button');
+    completedButton.innerHTML = '<i class="fas fa-check"></i>';
+    // notice this is innerHTML, not innerText
+    completedButton.classList.add('complete-btn');
+    todoDiv.appendChild(completedButton);
+
+    // check trash button
+    const trashButton = document.createElement('button');
+    trashButton.innerHTML = '<i class="fas fa-trash"></i>';
+    trashButton.classList.add('trash-btn');
+    todoDiv.appendChild(trashButton);
+
+
+
+
+    // append to list
+    todoList.appendChild(todoDiv);
+
+    // // clear todo input value
+    // todoInput.value = '';
+
+
 }
